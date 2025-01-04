a37be0f37d4e49ad9a4c1631211b4aec79fefbdd
RCUTANF <110910565+RCUTANF@users.noreply.github.com>
2025-01-04 13:23:00 +0800

    beautiful code style

--- fabric/src/client/java/com/hpfxd/spectatorplus/fabric/client/mixin/MinecraftMixin.java
+++ fabric/src/client/java/com/hpfxd/spectatorplus/fabric/client/mixin/MinecraftMixin.java
@@ -23,10 +23,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MinecraftMixin {
     @Inject(method = "setCameraEntity(Lnet/minecraft/world/entity/Entity;)V", at = @At(value = "TAIL"))
     private void spectatorplus$resetSyncDataOnCameraSwitch(Entity viewingEntity, CallbackInfo ci) {
-        if (ClientSyncController.syncData != null) {
-            if (viewingEntity == null || !ClientSyncController.syncData.playerId.equals(viewingEntity.getUUID())) {
-                ClientSyncController.setSyncData(null);
-            }
+        if (ClientSyncController.syncData != null && (viewingEntity == null || !ClientSyncController.syncData.playerId.equals(viewingEntity.getUUID()))) {
+            ClientSyncController.setSyncData(null);
         }
     }
 
