94b4d1c5a4280548299d78995206e5f561b425ce
Ricardo Cunha <cunhar@gmail.com>
2025-11-19 11:29:52 +0000

    Fix cloth config config

--- fabric/src/client/java/com/hpfxd/spectatorplus/fabric/client/config/ModMenuIntegration.java
+++ fabric/src/client/java/com/hpfxd/spectatorplus/fabric/client/config/ModMenuIntegration.java
@@ -3,15 +3,16 @@ package com.hpfxd.spectatorplus.fabric.client.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.gui.screens.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
-            return ClothConfigIntegration::getConfigScreen;
-        } else {
-            // cloth config isn't loaded, cannot provide config screen
+        return (Screen parent) -> {
+            if (FabricLoader.getInstance().isModLoaded("cloth-config")) {
+                return ClothConfigIntegration.getConfigScreen(parent);
+            }
             return null;
-        }
+        };
     }
 }
